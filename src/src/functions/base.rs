fe1780be474eaf1d8a759936c85095a4f6b357d5
Aditya Prakash Gupta <adityaprakashgupta726@gmail.com>
2025-07-07 18:42:51 +0530

    Removed some AxOS stuff

--- src/src/functions/base.rs
+++ src/src/functions/base.rs
@@ -33,19 +33,6 @@ pub fn install_base_packages(kernel: String) {
         "wget",
         "archlinux-keyring",
         "grep",
-        // Base AxOS
-        "about-axos",
-        "axos-hooks",
-        "axos-hooks-extra",
-        "plymouth-theme-axos",
-        "epsilon",
-        "axos-wallpaper-collection",
-        "grub-theme-axos",
-        "axctl",
-        // Extra goodies
-        "neofetch",
-        "axos/plymouth",
-        "plymouth-theme-axos",
         // Fonts
         "noto-fonts",
         "noto-fonts-cjk",
@@ -81,12 +68,9 @@ pub fn install_base_packages(kernel: String) {
         "unzip",
         // Graphic drivers
         "xf86-video-amdgpu",
-        "xf86-video-intel",
-        "xf86-video-nouveau",
         // "xf86-video-vmware",
         "xf86-video-vesa",
         "mesa",
-        "vulkan-intel",
         "vulkan-radeon",
         "vulkan-icd-loader",
         // "virtualbox-guest-utils",
@@ -95,7 +79,6 @@ pub fn install_base_packages(kernel: String) {
         "chaotic-mirrorlist",
         // Display manager
         "sddm",
-        "sddm-theme-axos",
     ]);
     files::copy_file("/etc/pacman.conf", "/mnt/etc/pacman.conf");
 
@@ -234,44 +217,63 @@ pub fn copy_live_config() {
 }
 
 pub fn install_nvidia() {
-    install(vec!["dkms", "nvidia", "nvidia-dkms", "nvidia-utils", "egl-wayland"]);
+    install(vec![
+        "dkms",
+        "nvidia",
+        "nvidia-dkms",
+        "nvidia-utils",
+        "egl-wayland",
+    ]);
 
     // Apply nvidia module in grub
     let grub_cmdline_content = std::fs::read_to_string("/mnt/etc/default/grub").unwrap_or_default();
     let mut grub_conf_found = false;
-    let mut lines: Vec<String> = grub_cmdline_content.lines().map(|line| {
-        if line.starts_with("GRUB_CMDLINE_LINUX_DEFAULT=") {
-            grub_conf_found = true;
-            if line.contains("nvidia-drm.modeset=1") {
-                line.to_string() // Already there, do nothing
+    let mut lines: Vec<String> = grub_cmdline_content
+        .lines()
+        .map(|line| {
+            if line.starts_with("GRUB_CMDLINE_LINUX_DEFAULT=") {
+                grub_conf_found = true;
+                if line.contains("nvidia-drm.modeset=1") {
+                    line.to_string() // Already there, do nothing
+                } else {
+                    line.replace(
+                        "GRUB_CMDLINE_LINUX_DEFAULT=\"",
+                        "GRUB_CMDLINE_LINUX_DEFAULT=\"nvidia-drm.modeset=1 ",
+                    )
+                }
             } else {
-                line.replace("GRUB_CMDLINE_LINUX_DEFAULT=\"", "GRUB_CMDLINE_LINUX_DEFAULT=\"nvidia-drm.modeset=1 ")
+                line.to_string()
             }
-        } else { line.to_string() }
-    }).collect();
-    if !grub_conf_found { lines.push("GRUB_CMDLINE_LINUX_DEFAULT=\"nvidia-drm.modeset=1\"".to_string()); }
+        })
+        .collect();
+    if !grub_conf_found {
+        lines.push("GRUB_CMDLINE_LINUX_DEFAULT=\"nvidia-drm.modeset=1\"".to_string());
+    }
     let new_grub_content = lines.join("\n");
     std::fs::write("/mnt/etc/default/grub", new_grub_content).unwrap();
 
     // Apply initcpio modules
-    let mkinitcpio_content = std::fs::read_to_string("/mnt/etc/mkinitcpio.conf").unwrap_or_default();
+    let mkinitcpio_content =
+        std::fs::read_to_string("/mnt/etc/mkinitcpio.conf").unwrap_or_default();
     let mut mkinitcpio_conf_found = false;
-    let mapped_lines: Vec<String> = mkinitcpio_content.lines().map(|line| {
-        if line.trim_start().starts_with("MODULES=") && !line.trim_start().starts_with("#") {
-            mkinitcpio_conf_found = true;
-            "MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)".to_string()
-        } else {
-            line.to_string()
-        }
-    }).collect();
-    
+    let mapped_lines: Vec<String> = mkinitcpio_content
+        .lines()
+        .map(|line| {
+            if line.trim_start().starts_with("MODULES=") && !line.trim_start().starts_with("#") {
+                mkinitcpio_conf_found = true;
+                "MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)".to_string()
+            } else {
+                line.to_string()
+            }
+        })
+        .collect();
+
     let mut final_lines = mapped_lines;
     if !mkinitcpio_conf_found {
         final_lines.push("MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)".to_string());
     }
     let new_initcpio_content = final_lines.join("\n");
     std::fs::write("/mnt/etc/mkinitcpio.conf", new_initcpio_content).unwrap();
-
 }
 
 pub fn enable_swap(size: u64) {
